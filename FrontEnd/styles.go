fc20c3d8012618b57dcbd4501857105d120b0812
Kledjo Malaj <kledjomalaj20@gmail.com>
2025-11-23 17:23:29 +0100

    Styles File

---
+++ FrontEnd/styles.go
@@ -0,0 +1,69 @@
+package main
+
+import "github.com/charmbracelet/lipgloss"
+
+func HomePageStyle1(termWidth int) lipgloss.Style {
+	return lipgloss.NewStyle().
+		Foreground(lipgloss.Color("210")).
+		PaddingRight(termWidth - 25).
+		BorderStyle(lipgloss.RoundedBorder()).
+		BorderForeground(lipgloss.Color("202")).
+		Align(lipgloss.Center)
+}
+
+func HomePageStyle2(termWidth, termHeight int) lipgloss.Style {
+	return lipgloss.NewStyle().
+		Foreground(lipgloss.Color("210")).
+		BorderStyle(lipgloss.RoundedBorder()).
+		BorderForeground(lipgloss.Color("202")).
+		Height(termHeight - 7).
+		PaddingRight(1).PaddingLeft(1).
+		Align(lipgloss.Center).
+		MarginLeft(2)
+}
+
+func OptionsStyle(termWidth int) lipgloss.Style {
+	return lipgloss.NewStyle().
+		Foreground(lipgloss.Color("210")).
+		BorderStyle(lipgloss.RoundedBorder()).
+		BorderForeground(lipgloss.Color("#3C3C3C")).
+		Width(termWidth - 25).
+		Padding(1)
+}
+
+var style4 = lipgloss.NewStyle().
+	Foreground(lipgloss.Color("205")).
+	Bold(true)
+
+var style5 = lipgloss.NewStyle().
+	Foreground(lipgloss.Color("150")).
+	Bold(true)
+
+var MethodStyle = lipgloss.NewStyle().
+	Foreground(lipgloss.Color("2")).
+	Bold(true)
+
+var UrlStyle = lipgloss.NewStyle().
+	Foreground(lipgloss.Color("4")).
+	Bold(true)
+
+var StatusOKStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("10"))   // Green
+var StatusErrorStyle = lipgloss.NewStyle().Foreground(lipgloss.Color("9")) // Red
+
+func InputStyle(termWidth int) lipgloss.Style {
+	return lipgloss.NewStyle().
+		Foreground(lipgloss.Color("210")).
+		Width(termWidth - 3).
+		Height(25).
+		BorderStyle(lipgloss.RoundedBorder()).
+		BorderForeground(lipgloss.Color("202"))
+
+}
+func ResponseStyle(termWidth int) lipgloss.Style {
+	return lipgloss.NewStyle().
+		Foreground(lipgloss.Color("210")).
+		BorderStyle(lipgloss.RoundedBorder()).
+		BorderForeground(lipgloss.Color("#3C3C3C")).
+		Width(termWidth - 3).
+		Padding(1)
+}
